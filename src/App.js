1da62e2e3e53160f938d4ce58c6b4dbc8013b0f9
DESKTOP-DS74KJ8\ChinhDo <chinhdnk@gmail.com>
2021-11-03 14:24:45 +0700

    add new source

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import {useState} from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
+
+function App() {
+  count[cartIsShown, setCartIsShown]=useState(false);
+  function showCartHandler(){
+    setCartIsShown(true);
+  }
+
+  function hideCartHandler(){
+    setCartIsShown(false);
+  }
+
+  return (
+    <CartProvider>
+      {cartIsShown && <div> Cart ... </div>}
+      <Header onShowCart={showCartHandler}/>
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
